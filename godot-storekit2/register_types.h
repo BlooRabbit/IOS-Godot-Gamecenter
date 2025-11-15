bca4127ac0200e38d7bd6254b626f8500e170715
Bloo <56018661+BlooRabbit@users.noreply.github.com>
2025-11-15 16:59:05 +0100

    new start

---
+++ godot-storekit2/register_types.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void register_godot_storekit2_types();
+void unregister_godot_storekit2_types();
---
+++ godot-storekit2/register_types.mm
@@ -0,0 +1,13 @@
+#include "register_types.h"
+
+#include "godot-storekit2.h"
+
+#include "core/object/class_db.h"
+
+void register_godot_storekit2_types() {
+	GDREGISTER_CLASS(GodotStoreKit2);
+}
+
+void unregister_godot_storekit2_types() {
+}
+
